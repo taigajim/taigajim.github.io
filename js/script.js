8eb919a60cc7258815e0f9e634d5abf5776962ac
taigajim <147197136+taigajim@users.noreply.github.com>
2024-09-26 21:05:01 +0300

    hover slider

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 window.addEventListener("load", () => {
-  const slidersContainer = document.getElementById("sliders-container");
+  const DEFAULT_PERCENTAGE = 50;
 
   class Slider {
     constructor(sliderElement) {
       this.slider = sliderElement;
       this.beforeImage = this.slider.querySelector(".before");
       this.afterImage = this.slider.querySelector(".after");
-      this.percentage = parseInt(this.slider.dataset.percentage) || 50;
+      this.percentage = parseInt(this.slider.dataset.percentage) || DEFAULT_PERCENTAGE;
       this.isDragging = false;
       this.animationFrame = null;
+      this.isHovering = false;
+      this.lastPercentage = this.percentage;
 
-      // Use a single bound method for both mouse and touch events
-      this.handleDragStartBound = this.handleDragStart.bind(this);
-      this.handleDragMoveBound = this.handleDragMove.bind(this);
-      this.handleDragEndBound = this.handleDragEnd.bind(this);
-      this.handleMouseMove = this.handleDragMove.bind(this);
-      this.handleMouseUp = this.handleDragEnd.bind(this);
-      this.handleResize = this.debounce(this.updateSlider.bind(this), 100);
-
+      this.initBoundMethods();
       this.init();
+    }
+
+    initBoundMethods() {
+      this.handleDragStart = this.handleDragStart.bind(this);
+      this.handleMove = this.handleMove.bind(this);
+      this.handleDragEnd = this.handleDragEnd.bind(this);
+      this.handleMouseEnter = this.handleMouseEnter.bind(this);
+      this.handleMouseLeave = this.handleMouseLeave.bind(this);
+      this.handleResize = this.debounce(this.updateSlider.bind(this), 100);
     }
 
     init() {
@@ -33,9 +37,6 @@ window.addEventListener("load", () => {
         .catch((error) => {
           console.error("Error loading images:", error);
           this.displayError();
-        })
-        .finally(() => {
-          this.checkAllSlidersReady(); //only for logging
         });
     }
 
@@ -54,56 +55,60 @@ window.addEventListener("load", () => {
       this.beforeImage.draggable = false;
       this.afterImage.draggable = false;
 
-      [this.slider, this.sliderLine].forEach((element) => {
-        element.addEventListener("mousedown", this.handleDragStartBound);
-        element.addEventListener("touchstart", this.handleDragStartBound);
-      });
+      this.addEvent(this.slider, "mousedown touchstart", this.handleDragStart);
+      this.addEvent(this.slider, "mousemove touchmove", this.handleMove, { passive: false });
+      this.addEvent(this.slider, "mouseenter", this.handleMouseEnter);
+      this.addEvent(this.slider, "mouseleave", this.handleMouseLeave);
+      this.addEvent(document, "mouseup touchend", this.handleDragEnd);
+      this.addEvent(window, "resize", this.handleResize);
 
-      window.addEventListener("mousemove", this.handleDragMoveBound);
-      window.addEventListener("touchmove", this.handleDragMoveBound);
-      window.addEventListener("mouseup", this.handleDragEndBound);
-      window.addEventListener("touchend", this.handleDragEndBound);
-      window.addEventListener("resize", this.handleResize);
+      this.slider.dataset.initialPercentage = this.slider.dataset.percentage || DEFAULT_PERCENTAGE;
+    }
+
+    addEvent(element, events, handler, options = {}) {
+      events.split(" ").forEach((event) => {
+        element.addEventListener(event, handler, options);
+      });
     }
 
     handleDragStart(e) {
-      // e.preventDefault(); // Remove this line
+      e.preventDefault();
       this.isDragging = true;
-
-      const clientX = e.type.includes("touch") ? e.touches[0].clientX : e.clientX;
-      const rect = this.slider.getBoundingClientRect();
-      let percentage = ((clientX - rect.left) / rect.width) * 100;
-      percentage = Math.max(0, Math.min(100, percentage));
-
-      this.animateTo(percentage, 150);
+      if (e.pointerId) this.slider.setPointerCapture(e.pointerId);
+      this.handleMove(e);
     }
 
-    handleDragMove(e) {
-      if (!this.isDragging) return;
-
-      if (this.animationFrame) {
-        cancelAnimationFrame(this.animationFrame);
-        this.animationFrame = null;
-      }
+    handleMove(e) {
+      if (e.type !== "mousemove" && !this.isDragging) return;
+      e.preventDefault();
 
       const clientX = e.type.includes("touch") ? e.touches[0].clientX : e.clientX;
       const rect = this.slider.getBoundingClientRect();
       let percentage = ((clientX - rect.left) / rect.width) * 100;
       percentage = Math.max(0, Math.min(100, percentage));
-      this.percentage = percentage;
+
+      this.percentage = this.lastPercentage = percentage;
       this.slider.dataset.percentage = percentage;
       this.updateSlider();
     }
 
-    handleDragEnd() {
+    handleMouseEnter() {
+      this.isHovering = true;
+    }
+
+    handleMouseLeave() {
+      this.isHovering = false;
+    }
+
+    handleDragEnd(e) {
       this.isDragging = false;
+      if (e.pointerId) this.slider.releasePointerCapture(e.pointerId);
     }
 
     animateTo(targetPercentage, duration = 150) {
       const startPercentage = this.percentage;
       const change = targetPercentage - startPercentage;
       const startTime = performance.now();
-
       const easeOutCubic = (t) => 1 - Math.pow(1 - t, 3);
 
       const animate = (currentTime) => {
@@ -122,9 +127,7 @@ window.addEventListener("load", () => {
         }
       };
 
-      if (this.animationFrame) {
-        cancelAnimationFrame(this.animationFrame);
-      }
+      if (this.animationFrame) cancelAnimationFrame(this.animationFrame);
       this.animationFrame = requestAnimationFrame(animate);
     }
 
@@ -140,7 +143,6 @@ window.addEventListener("load", () => {
             requestAnimationFrame(checkImages);
           }
         };
-
         checkImages();
       });
     }
@@ -150,7 +152,6 @@ window.addEventListener("load", () => {
       this.slider.innerHTML += "<p style='color: red;'>Error loading images. Please refresh the page.</p>";
     }
 
-    // Debounce utility function
     debounce(func, wait) {
       let timeout;
       return function (...args) {
@@ -160,21 +161,21 @@ window.addEventListener("load", () => {
     }
 
     destroy() {
-      [this.slider, this.sliderLine].forEach((element) => {
-        element.removeEventListener("mousedown", this.handleDragStartBound);
-        element.removeEventListener("touchstart", this.handleDragStartBound);
+      this.removeEvent(this.slider, "mousedown touchstart", this.handleDragStart);
+      this.removeEvent(this.slider, "mousemove touchmove", this.handleMove);
+      this.removeEvent(this.slider, "mouseenter", this.handleMouseEnter);
+      this.removeEvent(this.slider, "mouseleave", this.handleMouseLeave);
+      this.removeEvent(document, "mouseup touchend", this.handleDragEnd);
+      this.removeEvent(window, "resize", this.handleResize);
+    }
+
+    removeEvent(element, events, handler) {
+      events.split(" ").forEach((event) => {
+        element.removeEventListener(event, handler);
       });
-
-      window.removeEventListener("mousemove", this.handleDragMoveBound);
-      window.removeEventListener("touchmove", this.handleDragMoveBound);
-      window.removeEventListener("mouseup", this.handleDragEndBound);
-      window.removeEventListener("touchend", this.handleDragEndBound);
-
-      window.removeEventListener("resize", this.handleResize);
     }
   }
 
-  // Initialize Sliders
   function initializeSliders() {
     console.log("Initializing sliders");
     const sliders = document.querySelectorAll(".image-slider");
